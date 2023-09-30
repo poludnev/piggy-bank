fd3e5c6af444bbbffd611ee418e9c1b7804d3356
poludnev <poludnev.roman@gmail.com>
2023-09-30 15:32:31 +0200

    Basic settings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import Head from 'next/head';
 
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { SSRConfig, UseTranslation, UserConfig, useTranslation } from 'next-i18next';
-import type { InferGetStaticPropsType, GetStaticProps } from 'next';
+import { useTranslation } from 'next-i18next';
+import type { GetStaticProps } from 'next';
 
-import styles from '@/styles/Home.module.css';
+import styles from '@/styles/Home.module.scss';
 import { useEffect } from 'react';
 
 export const getStaticProps = (async (contex) => {
@@ -20,8 +20,6 @@ export default function Home() {
   const { t } = useTranslation(['common']);
 
   useEffect(() => {
-    console.log('index useEffect');
-    console.log('run fetch');
     fetch('/api/hello')
       .then((res) => res.text())
       .then((data) => {
@@ -40,7 +38,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main}`}>Piggy bank</main>
+      <main className={`${styles.main}`}>
+        <h1>{t('title')}</h1>
+      </main>
     </>
   );
 }
